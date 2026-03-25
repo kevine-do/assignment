215f468030c681f98f3fd34bcbd7b6072df7ebb5
kevine-do <kabalirakevine9@gmail.com>
2026-03-25 15:23:01 +0200

    Add files via upload

---
+++ example one if condition.py
@@ -0,0 +1,87 @@
+# Registered = input ("are you registered for exam (yes/no):  ")
+# school_fee = input("are you Tution Paid Already?:): (yes/no:")
+                   
+# if Registered == "yes"and school_fee =="yes":
+#     print("you're cleared")
+# elif Registered == "yes" and school_fee =="no":
+#    print("you not allowed into exam")
+# else: 
+#   print("you are not allowed to come to school")
+
+
+
+
+# number = float(input("ENTER THE NUMBER OF YOUR CHOICE!"))
+# if number > 0:
+#    print("the NUMBER IS POSITIVE!")
+# else:
+#    print("THE NUMBER IS NEUTRAL")
+
+         # grading system simulation
+
+# score = float(input("Enter the your score"))
+# if score >= 80:
+#     print("This is Grade A")
+# elif score >= 70:
+#     print("This is Grade B")
+# elif score>= 60:
+#     print(" this is Grade C")
+# elif score >= 50:
+    # print("this is Grade D")
+# else:
+#     print("You have failed")
+
+
+           # salary increment simulation
+
+# Monthly_salary = float(input("Enter your Monthly_salary"))
+# experience = float(input("Enter your years of experience"))
+# if experience >= 10:
+#     bonus = Monthly_salary*0.5
+#     print("Your bonus is:", bonus)
+# elif experience >= 5:
+#     bonus = Monthly_salary*0.2
+#     print("Your bonus is:", bonus)
+# else:
+#     bonus = 0
+#     print("Your bonus is:", bonus)
+
+# from xxlimited import new
+
+
+# salary = float(input("ENTER THE WORKER SALARY:"))
+# experience  = int(input("ENTER THE EXPERIENCE"))
+
+# if experience>=10:
+#      new_salary = salary + 0.5*salary
+#      print(f"your new salary is {new_salary}rwf") 
+# elif experience>=5:
+#      new_salary = salary + 0.2*salary
+#      print(f"your new salary is {new_salary} rwf ")
+# else:
+#      print(f"your salary is{salary} rwf")
+#      another example of salary increment
+
+# salary = float(input("enter your current salary:"))
+# year_experience = int(input("enter the years of experience:"))
+
+# if year_experience >=10:
+#     increment = salary*4
+# elif year_experience >=5:
+#     increment =salary*2
+# else:
+#     increment = salary *1
+#     print("increment:",increment)
+# from unittest import case
+
+
+# day=int(input("enter the day of the week (1-2):"))
+# match day:
+#   case 1:
+#         print("moday")
+#         case 2:
+#         print("tuesday")
+#         case _:
+#         print("error")
+
+
